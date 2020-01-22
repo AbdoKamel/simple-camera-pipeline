066d7412d06b6f66659bcd94b31ae428cb353c48
ABDOT440S\Abdo <abdoukamel@gmail.com>
2020-01-22 10:51:29 -0500

    Prep for python version.

--- CropActiveArea.m
+++
@@ -1,8 +0,0 @@
-function [ croppedRaw ] = CropActiveArea(raw,  metadata )
-
-activeArea = GetActiveArea(metadata);
-croppedRaw = raw(activeArea(1):activeArea(3), ...
-          activeArea(2):activeArea(4));
-   
-end
-
--- GetNLF.m
+++
@@ -1,6 +0,0 @@
-function [nlf] = GetNLF(metadata)
-% Extracts noise level function (NLF) from DNG metadata
-
-nlf = metadata.UnknownTags(8).Value;
-
-end
