0ed0af74be497888156def66217a10d0e9638448
Abdo <abdoukamel@gmail.com>
2020-03-16 12:27:10 -0400

    Search for noise level function (tag key 51041) in all unknown tags.

--- matlab/GetNLF.m
+++ matlab/GetNLF.m
@@ -1,6 +1,23 @@
 function [nlf] = GetNLF(metadata)
 % Extracts noise level function (NLF) from DNG metadata
 
-nlf = metadata.UnknownTags(8).Value;
+tags = metadata.UnknownTags;
+found = 0;
+
+for i = 1 : numel(tags)
+    if tags(i).ID == 51041
+        nlf = tags(i).Value;
+        found = 1;
+    end
+end
+
+if found 
+    if numel(nlf) == 2
+        nlf(3:4) = nlf(1:2);
+        nlf(5:6) = nlf(1:2);
+    end
+else 
+    disp('NLF not found.');
+end
 
 end
