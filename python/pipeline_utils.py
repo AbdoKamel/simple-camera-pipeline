a79cab3d627f2d00a07c5c44f097f104b8a87555
ABDOT440S\Abdo <abdoukamel@gmail.com>
2020-05-13 18:00:41 -0400

    Selecting OpenCV demosaicing algorithm.

--- python/pipeline_utils.py
+++ python/pipeline_utils.py
@@ -139,39 +139,54 @@ def white_balance(normalized_image, as_shot_neutral, cfa_pattern):
     return white_balanced_image
 
 
-def get_opencv_demsaic_flag(cfa_pattern, output_channel_order):
+def get_opencv_demsaic_flag(cfa_pattern, output_channel_order, alg_type='VNG'):
     # using opencv edge-aware demosaicing
+    if alg_type != '':
+        alg_type = '_' + alg_type
     if output_channel_order == 'BGR':
         if cfa_pattern == [0, 1, 1, 2]:  # RGGB
-            opencv_demosaic_flag = cv2.COLOR_BAYER_BG2BGR_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_BG2BGR' + alg_type)
         elif cfa_pattern == [2, 1, 1, 0]:  # BGGR
-            opencv_demosaic_flag = cv2.COLOR_BAYER_RG2BGR_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_RG2BGR' + alg_type)
         elif cfa_pattern == [1, 0, 2, 1]:  # GRBG
-            opencv_demosaic_flag = cv2.COLOR_BAYER_GB2BGR_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_GB2BGR' + alg_type)
         elif cfa_pattern == [1, 2, 0, 1]:  # GBRG
-            opencv_demosaic_flag = cv2.COLOR_BAYER_GR2BGR_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_GR2BGR' + alg_type)
         else:
-            opencv_demosaic_flag = cv2.COLOR_BAYER_BG2BGR_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_BG2BGR' + alg_type)
             print("CFA pattern not identified.")
     else:  # RGB
         if cfa_pattern == [0, 1, 1, 2]:  # RGGB
-            opencv_demosaic_flag = cv2.COLOR_BAYER_BG2RGB_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_BG2RGB' + alg_type)
         elif cfa_pattern == [2, 1, 1, 0]:  # BGGR
-            opencv_demosaic_flag = cv2.COLOR_BAYER_RG2RGB_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_RG2RGB' + alg_type)
         elif cfa_pattern == [1, 0, 2, 1]:  # GRBG
-            opencv_demosaic_flag = cv2.COLOR_BAYER_GB2RGB_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_GB2RGB' + alg_type)
         elif cfa_pattern == [1, 2, 0, 1]:  # GBRG
-            opencv_demosaic_flag = cv2.COLOR_BAYER_GR2RGB_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_GR2RGB' + alg_type)
         else:
-            opencv_demosaic_flag = cv2.COLOR_BAYER_BG2RGB_EA
+            opencv_demosaic_flag = eval('cv2.COLOR_BAYER_BG2RGB' + alg_type)
             print("CFA pattern not identified.")
     return opencv_demosaic_flag
 
 
-def demosaic(white_balanced_image, cfa_pattern, output_channel_order='BGR'):
-    opencv_demosaic_flag = get_opencv_demsaic_flag(cfa_pattern, output_channel_order)
-    max_val = 16383
-    demosaiced_image = cv2.cvtColor((white_balanced_image * max_val).astype(dtype=np.uint16), opencv_demosaic_flag)
+def demosaic(white_balanced_image, cfa_pattern, output_channel_order='BGR', alg_type='VNG'):
+    """
+    Demosaic a Bayer image.
+    :param white_balanced_image:
+    :param cfa_pattern:
+    :param output_channel_order:
+    :param alg_type: algorithm type. options: '', 'EA' for edge-aware, 'VNG' for variable number of gradients
+    :return: Demosaiced image
+    """
+    if alg_type == 'VNG':
+        max_val = 255
+        wb_image = (white_balanced_image * max_val).astype(dtype=np.uint8)
+    else:
+        max_val = 16383
+        wb_image = (white_balanced_image * max_val).astype(dtype=np.uint16)
+    opencv_demosaic_flag = get_opencv_demsaic_flag(cfa_pattern, output_channel_order, alg_type=alg_type)
+    demosaiced_image = cv2.cvtColor(wb_image, opencv_demosaic_flag)
     demosaiced_image = demosaiced_image.astype(dtype=np.float32) / max_val
     return demosaiced_image
 
@@ -265,3 +280,20 @@ def apply_tone_map(x):
     x = np.round(x * (len(tone_curve) - 1)).astype(int)
     tone_mapped_image = np.squeeze(tone_curve[x])
     return tone_mapped_image
+
+
+def raw_rgb_to_cct(rawRgb, xyz2cam1, xyz2cam2):
+    """Convert raw-RGB triplet to corresponding correlated color temperature (CCT)"""
+    pass
+    # pxyz = [.5, 1, .5]
+    # loss = 1e10
+    # k = 1
+    # while loss > 1e-4:
+    #     cct = XyzToCct(pxyz)
+    #     xyz = RawRgbToXyz(rawRgb, cct, xyz2cam1, xyz2cam2)
+    #     loss = norm(xyz - pxyz)
+    #     pxyz = xyz
+    #     fprintf('k = %d, loss = %f\n', [k, loss])
+    #     k = k + 1
+    # end
+    # temp = cct
