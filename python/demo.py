ff38f8a8e5c678cb6e9d65f18faed25e981d5ba0
ABDOT440S\Abdo <abdoukamel@gmail.com>
2020-05-13 16:46:15 -0400

    Added a 'save_as' option.

--- python/demo.py
+++ python/demo.py
@@ -5,9 +5,9 @@ import numpy as np
 
 from python.pipeline import run_pipeline
 
-
 params = {
     'output_stage': 'srgb',  # options: 'normal', 'white_balance', 'demosaic', 'xyz', 'srgb', 'gamma', 'tone'
+    'save_as': 'jpg',  # options: 'jpg', 'png', 'tif', etc.
 }
 
 # processing a directory
@@ -15,6 +15,9 @@ images_dir = '../data/'
 image_paths = glob.glob(os.path.join(images_dir, '*.dng'))
 for image_path in image_paths:
     output_image = run_pipeline(image_path, params)
-    output_image_path = image_path.replace('.dng', '_processed.jpg')
+    output_image_path = image_path.replace('.dng', '_processed.' + params['save_as'])
     output_image = (output_image * 255).astype(np.uint8)
-    cv2.imwrite(output_image_path, output_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    if params['save_as'] == 'jpg':
+        cv2.imwrite(output_image_path, output_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    else:
+        cv2.imwrite(output_image_path, output_image)
