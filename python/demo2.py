dacd1e5527f2fa0833aff5bd6f4e210d0155aa2a
Abdelrahman Abdelhamed <abdoukamel@gmail.com>
2021-11-01 11:37:08 -0400

    Fix: max_val.
    Signed-off-by: Abdelrahman Abdelhamed <abdoukamel@gmail.com>

--- python/demo2.py
+++ python/demo2.py
@@ -10,7 +10,7 @@ params = {
     'input_stage': 'raw',  # options: 'raw', 'normal', 'white_balance', 'demosaic', 'xyz', 'srgb', 'gamma', 'tone'
     'output_stage': 'tone',  # options: 'normal', 'white_balance', 'demosaic', 'xyz', 'srgb', 'gamma', 'tone'
     'save_as': 'png',  # options: 'jpg', 'png', 'tif', etc.
-    'demosaic_type': 'menon2007',
+    'demosaic_type': 'EA',
     'save_dtype': np.uint16
 }
 
@@ -34,7 +34,7 @@ for image_path in image_paths:
     # save
     output_image_path = image_path.replace('.dng', '_{}.'.format(params['output_stage']) + params['save_as'])
     max_val = 2 ** 16 if params['save_dtype'] == np.uint16 else 255
-    output_image = (output_image[..., ::-1] * 255).astype(params['save_dtype'])
+    output_image = (output_image[..., ::-1] * max_val).astype(params['save_dtype'])
     if params['save_as'] == 'jpg':
         cv2.imwrite(output_image_path, output_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
     else:
